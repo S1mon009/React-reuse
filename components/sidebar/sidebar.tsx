5f87b74f96a6afbf4c3c51b813ffafb6f151f636
S1mon009 <skaszy25@gmail.com>
2024-08-17 14:56:24 +0200

    Modify github actions

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -10,25 +10,23 @@ interface sidebarContentProps extends sidebarProps {}
 interface sidebarHeaderProps extends sidebarProps {}
 
 export function Sidebar({ className, children }: sidebarProps): JSX.Element {
-  return <div className={cn("w-full", className)}>{children}</div>;
+  return <div className={cn("w-full px-2 pb-2", className)}>{children}</div>;
 }
 
 export function SidebarContent({
   children,
   className,
 }: sidebarContentProps): JSX.Element {
-  return <div className={className}>{children}</div>;
+  return (
+    <div className={cn("[&:not(:first-child)]:ml-2", className)}>
+      {children}
+    </div>
+  );
 }
 
 export function SidebarHeader({
   children,
   className,
 }: sidebarHeaderProps): JSX.Element {
-  return (
-    <div
-      className={`font-semibold [&:not(:first-child)]:py-2 [&:first-child]:pb-2 ${className}`}
-    >
-      {children}
-    </div>
-  );
+  return <div className={`font-semibold py-2 ${className}`}>{children}</div>;
 }
